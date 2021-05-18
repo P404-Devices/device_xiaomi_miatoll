6d03842e8a48dfe0e8eaeeebb192af350694db3d
Arjun-Ingole <arjuningole38@gmail.com>
2021-05-18 10:14:54 +0000

    miatoll : Project 404 Bring Up

--- p404_miatoll.mk
+++ p404_miatoll.mk
@@ -5,9 +5,12 @@
 #
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
-PRODUCT_NAME := lineage_miatoll
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := p404_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
